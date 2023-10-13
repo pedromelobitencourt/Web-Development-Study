c8cbec26727e5233056200dcaac27e07e0b5c38e
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2023-10-13 17:41:22 -0300

    HTML

--- HTML/exercises/forms/server.js
+++ HTML/exercises/forms/server.js
@@ -8,7 +8,13 @@ app.use(bodyParser.urlencoded({ extended: true })); // To get the body
 
 app.post('/users', (req, res) => {
     console.log(req.body); // We'll get { namee: 'input' }; name because 'name="namee"'
-    res.send("<h1>Congratulations!</h1>"); // The page will load this
+    res.send("<h1>User added!</h1>"); // The page will load this
+});
+
+app.post('/users/:id', (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body); // We'll get { namee: 'input' }; name because 'name="namee"'
+    res.send("<h1>User updated</h1>"); // The page will load this
 });
 
 app.listen(3003);
