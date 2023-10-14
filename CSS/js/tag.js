194b4b81bdd82a7396147988cd3bb3d15be175b9
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2023-10-14 12:38:51 -0300

    CSS

---
+++ CSS/js/tag.js
@@ -0,0 +1,33 @@
+const colors = {
+    p: '#388e3c',
+    div: '#1565c0',
+    span: '#e53935',
+    section: '#f67809',
+    ul: '#5e35b1',
+    ol: '#fbc02d',
+    header: '#d81b60',
+    nav: '#64dd17',
+    main: '#00acc1',
+    footer: '#304ffe',
+    form: '#9f6581',
+    body: '#25b6da',
+    pattern: '616161',
+    get(tag) {
+        return this[tag] ? this[tag] : this[pattern];
+    }
+}
+
+document.querySelectorAll('.tag').forEach(element => {
+    const tagName = element.tagName.toLowerCase();
+
+    console.log(tagName);
+
+    element.style.borderColor = colors.get(tagName);
+
+    if(!element.classList.contains('nolabel')) {
+        const label = document.createElement('label');
+        label.style.backgroundColor = colors.get(tagName);
+        label.innerHTML = tagName;
+        element.insertBefore(label, element.childNodes[0]);
+    }
+});
